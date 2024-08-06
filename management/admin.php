07c1c84ba7467ba5d2b27af2598d3fc563a14c2d
Sajit Maharjan <126838978+Sajit21@users.noreply.github.com>
2024-08-06 18:32:03 +0545

    Update admin.php

--- management/admin.php
+++ management/admin.php
@@ -1,5 +1,5 @@
 // we have edit ,delete option and edit option is available when we click on edit button
-
+//include access to the admin page
 <?php
 require_once("entrycheck_admin.php");
 require('dbcopy.php');
